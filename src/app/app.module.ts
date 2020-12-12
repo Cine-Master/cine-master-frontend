3a4f24c52a417be5f01b8b1359700d233e78b032
Lorenzo <46032643+LeRoy19@users.noreply.github.com>
2020-12-12 16:10:22 +0100

    Created login and dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'admin/dashboard', component: DashboardComponent},
+      {path: 'login', component: LoginComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
