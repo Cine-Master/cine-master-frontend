a5a5c8b42c349b0c549d944e5737ce2ea4683246
lucalabo <merda98>
2020-12-14 21:50:14 +0100

    Manage Shows component.

---
+++ src/app/admin/dashboard/manage-shows/datasource.ts
@@ -0,0 +1,6 @@
+// tslint:disable-next-line:ban-types
+export let data: Object[] = [
+  {
+    OrderID: 10248, CustomerID: 'VINET', Freight: 32.38,  OrderDate: new Date(8364186e5),
+    ShipCountry: 'Italy'
+  }];
---
+++ src/app/admin/dashboard/manage-shows/manage-shows.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { EditService, PageService, CommandColumnService, CommandModel } from '@syncfusion/ej2-angular-grids';
+import {data} from './datasource';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'ej-grid-manageshowscomponent',
+  templateUrl: './manage-shows.component.html',
+  styleUrls: ['./manage-shows.component.css']
+})
+export class ManageShowsComponent implements OnInit {
+  public data: object[];
+  public editSettings: object;
+  public orderidrules: object;
+  public customeridrules: object;
+  public freightrules: object;
+  public editparams: object;
+  public pageSettings: object;
+  public commands: CommandModel[];
+
+  public ngOnInit(): void {
+    this.data = data;
+    console.log(data);
+    this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal', allowEditOnDblClick: false };
+    this.orderidrules = { required: true };
+    this.customeridrules = { required: true };
+    this.freightrules =  { required: true };
+    this.editparams = { params: { popupHeight: '300px' }};
+    this.pageSettings = {pageCount: 5};
+    this.commands = [{ type: 'Edit', buttonOption: { iconCss: ' e-icons e-edit', cssClass: 'e-flat' } },
+      { type: 'Delete', buttonOption: { iconCss: 'e-icons e-delete', cssClass: 'e-flat' } },
+      { type: 'Save', buttonOption: { iconCss: 'e-icons e-update', cssClass: 'e-flat' } },
+      { type: 'Cancel', buttonOption: { iconCss: 'e-icons e-cancel-icon', cssClass: 'e-flat' } }];
+  }
+}
