b7119bb8f50f595c59522db326d52d51e8248bc1
lucalabo <merda98>
2020-12-14 19:50:23 +0100

    Commit iniziale per aggiunta componente "gestione spettacoli".

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { MenuModule, SidebarModule } from '@syncfusion/ej2-angular-navigations';
+import { ManageShowsComponent } from './admin/dashboard/manage-shows/manage-shows.component';
+import {CommandColumnService, EditService, GridModule, PageService} from '@syncfusion/ej2-angular-grids';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
+    ManageShowsComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'admin/dashboard', component: DashboardComponent},
-      {path: 'login', component: LoginComponent}
+      {path: 'login', component: LoginComponent},
+      {path: 'admin/dashboard/manage_shows', component: ManageShowsComponent}
     ]),
     ReactiveFormsModule,
     HttpClientModule,
     MenuModule,
-    SidebarModule
+    SidebarModule,
+    GridModule,
   ],
-  providers: [],
+  providers: [CommandColumnService, EditService, PageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
