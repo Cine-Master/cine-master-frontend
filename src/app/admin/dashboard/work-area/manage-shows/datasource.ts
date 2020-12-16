252d5a4fbe40e38300dc3a64d9c8b85a7597993c
lucalabo <merda98>
2020-12-16 21:05:59 +0100

    Cambiata impostazione tabella e aggiunta alert sulla rimozione dello spettacolo.

--- src/app/admin/dashboard/work-area/manage-shows/datasource.ts
+++ src/app/admin/dashboard/work-area/manage-shows/datasource.ts
@@ -1,6 +1,9 @@
 // tslint:disable-next-line:ban-types
+
+// tslint:disable-next-line:ban-types
 export let data: Object[] = [
   {
-    OrderID: 10248, CustomerID: 'VINET', Freight: 32.38,  OrderDate: new Date(8364186e5),
-    ShipCountry: 'Italy'
+    // tslint:disable-next-line:max-line-length
+    Title: 'Spiderman 3', Description: 'Spiderma3 è il terzo film della trilogia che racconterà le gesta del supereroe', Cover: 'DAGESTIRE',  Date: new Date(8364186e5),
+    StartTime: '15:20', EndTime: '17:30' , Nationality: 'Italia'
   }];
--- src/app/admin/dashboard/work-area/manage-shows/manage-shows.component.ts
+++ src/app/admin/dashboard/work-area/manage-shows/manage-shows.component.ts
@@ -2,6 +2,20 @@ import {Component, Input, OnInit} from '@angular/core';
 import { EditService, PageService, CommandColumnService, CommandModel } from '@syncfusion/ej2-angular-grids';
 import {DashboardItem} from '../item/dashboard-item';
 import {ItemComponent} from '../item/item.component';
+import { L10n, setCulture } from '@syncfusion/ej2-base';
+
+setCulture('en-US');
+L10n.load({
+  'en-US': {
+    grid: {
+      ConfirmDelete : 'Sicuro di voler rimuovere lo spettacolo?'
+    },
+    pager: {
+      currentPageInfo: '{0} Di {1} Pagine',
+      totalItemsInfo: '({0} Oggetti)',
+    }
+  }
+});
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -12,23 +26,25 @@ import {ItemComponent} from '../item/item.component';
 export class ManageShowsComponent implements OnInit, ItemComponent {
   @Input() data: object[];
   public editSettings: object;
-  public orderidrules: object;
-  public customeridrules: object;
-  public freightrules: object;
+  public titlerules: object;
+  public descriptionrules: object;
+  public coverrules: object;
   public editparams: object;
   public pageSettings: object;
   public commands: CommandModel[];
+  it: any;
+  IT: any;
 
   public ngOnInit(): void {
-    console.log(this.data);
-    this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal', allowEditOnDblClick: false };
-    this.orderidrules = { required: true };
-    this.customeridrules = { required: true };
-    this.freightrules =  { required: true };
-    this.editparams = { params: { popupHeight: '300px' }};
+    // tslint:disable-next-line:max-line-length
+    this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal', allowEditOnDblClick: false, showDeleteConfirmDialog: true };
+    this.titlerules = { required: true };
+    this.descriptionrules = { required: true };
+    this.coverrules =  { required: true };
+    this.editparams = {  params: { popupHeight: '300px' }};
     this.pageSettings = {pageCount: 5};
     this.commands = [{ type: 'Edit', buttonOption: { iconCss: ' e-icons e-edit', cssClass: 'e-flat' } },
-      { type: 'Delete', buttonOption: { iconCss: 'e-icons e-delete', cssClass: 'e-flat' } },
+      { type: 'Delete', buttonOption: { iconCss: 'e-icons e-delete', cssClass: 'e-flat' }  },
       { type: 'Save', buttonOption: { iconCss: 'e-icons e-update', cssClass: 'e-flat' } },
       { type: 'Cancel', buttonOption: { iconCss: 'e-icons e-cancel-icon', cssClass: 'e-flat' } }];
   }
