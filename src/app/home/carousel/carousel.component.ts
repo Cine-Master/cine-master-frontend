82ff4654e6a947f0d76f31932431cd3910f9a79d
Giorgio Andronico <bytecodex2@gmail.com>
2020-12-23 12:56:49 +0100

    Carousel graphics fixed.

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -1,7 +1,8 @@
-import { Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 
-@Component({selector: 'app-carousel', templateUrl: './carousel.component.html'})
+@Component({selector: 'app-carousel',  encapsulation: ViewEncapsulation.None
+  , styleUrls: ['./carousel.component.css'], templateUrl: './carousel.component.html'})
 export class CarouselComponent {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
