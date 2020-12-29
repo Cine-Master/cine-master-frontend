dea652dee944a77c9bee55bb5c129d5ceb7fdc75
Giorgio Andronico <bytecodex2@gmail.com>
2020-12-29 18:48:30 +0100

    Added spinner to show while carousel is not ready.

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {SearchService} from '../../services/search.service';
 
 
@@ -6,10 +6,13 @@ import {SearchService} from '../../services/search.service';
   encapsulation: ViewEncapsulation.None
   , styleUrls: ['./carousel.component.css'],
   templateUrl: './carousel.component.html'})
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements OnInit, AfterViewInit {
   // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
   searchResults: any;
+  public loaded = false;
+
+
 
   constructor(private searchService: SearchService) {
     this.searchService = searchService;
@@ -17,9 +20,16 @@ export class CarouselComponent implements OnInit {
 
   ngOnInit(): void {
 
+
+
+  }
+
+  ngAfterViewInit(): void {
+
     this.searchService.getHighlightedShows().subscribe(response => {
       this.searchResults = response;
       console.log(response);
+      this.loaded = true;
     }, error => {});
 
   }
