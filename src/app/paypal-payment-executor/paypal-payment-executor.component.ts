147b3a8bbe0a1b33611938c566adc2b5462fc90f
antonioagostino <antony_97-@hotmail.it>
2020-12-31 14:14:43 +0100

    Implemented PayPal Payments Support

---
+++ src/app/paypal-payment-executor/paypal-payment-executor.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import {ICreateOrderRequest, IPayPalConfig} from 'ngx-paypal';
+
+@Component({
+  selector: 'app-paypal-payment-executor',
+  templateUrl: './paypal-payment-executor.component.html',
+  styleUrls: ['./paypal-payment-executor.component.css']
+})
+export class PaypalPaymentExecutorComponent implements OnInit {
+
+  public payPalConfig?: IPayPalConfig;
+  showSuccess: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initConfig();
+  }
+
+  private initConfig(): void {
+    this.payPalConfig = {
+      currency: 'EUR',
+      clientId: 'AUuQsZ6W_kSfRMWY_JWNer6Ho-eU3XDcVAgce3CZYj8LhYJO4ZiL9AME6LMbWHPxGkbTVp3zHpoQudsR',
+      createOrderOnClient: (data) => <ICreateOrderRequest>{
+        intent: 'CAPTURE',
+        purchase_units: [
+          {
+            amount: {
+              currency_code: 'EUR',
+              value: '9.99',
+              breakdown: {
+                item_total: {
+                  currency_code: 'EUR',
+                  value: '9.99'
+                }
+              }
+            },
+            items: [
+              {
+                name: 'Enterprise Subscription',
+                quantity: '1',
+                category: 'DIGITAL_GOODS',
+                unit_amount: {
+                  currency_code: 'EUR',
+                  value: '9.99',
+                },
+              }
+            ]
+          }
+        ]
+      },
+      advanced: {
+        commit: 'true'
+      },
+      style: {
+        label: 'paypal',
+        layout: 'vertical'
+      },
+      onApprove: (data, actions) => {
+        console.log('onApprove - transaction was approved, but not authorized', data, actions);
+        actions.order.get().then(details => {
+          console.log('onApprove - you can get full order details inside onApprove: ', details);
+        });
+      },
+      onClientAuthorization: (data) => {
+        console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+        this.showSuccess = true;
+      },
+      onCancel: (data, actions) => {
+        console.log('OnCancel', data, actions);
+      },
+      onError: err => {
+        console.log('OnError', err);
+      },
+      onClick: (data, actions) => {
+        console.log('onClick', data, actions);
+      },
+    };
+  }
+
+}
