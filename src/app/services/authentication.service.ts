229cd24447ec187b85f6ceb0572d63269e47312a
Giorgio Andronico <bytecodex2@gmail.com>
2021-01-04 19:25:45 +0100

    Logout fixed

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -13,14 +13,24 @@ export class AuthenticationService {
   constructor(private httpClient: HttpClient) { }
 
   authenticateUser(loginData): any {
+    console.log(loginData);
     // TODO concordare URL e dati restituiti da REST
     return this.httpClient.post<string>('http://localhost:8080/login', {username: loginData.username,
       password: loginData.password}, { withCredentials: true });
   }
+
+  authenticateUserBis(user,pass): any {
+    // TODO concordare URL e dati restituiti da REST
+    return this.httpClient.post<string>('http://localhost:8080/login', {username: user,
+      password: pass}, { withCredentials: true });
+  }
+
   registrationUser(registrationData): any{
     return this.httpClient.post<string>('http://localhost:8080/registration', {username: registrationData.username, firstName: registrationData.firstname, hashedPassword: registrationData.password,
-      // tslint:disable-next-line:max-line-length
       lastName: registrationData.lastname, email: registrationData.email, birthdate: registrationData.birthdate, gender: registrationData.gender}, { withCredentials: true });
   }
+
+  logoutUser(): any {
+    return this.httpClient.request('post', 'http://localhost:8080/logout', {body: {}, withCredentials: true, responseType: 'text' });  }
 
 }
